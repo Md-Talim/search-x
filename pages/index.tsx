adfbef3c2e0e619d4d0c86e66fff024acbe79d06
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-30 19:31:51 +0530

    Add the Input field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <>
-      
+      <main className='w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <form className=''>
+          <input
+            placeholder='Search...'
+            className='block px-6 py-4 rounded-lg w-1/3 mx-auto shadow-lg focus:outline-none text-[#777]'
+          />
+        </form>
+      </main>
     </>
   );
 }
