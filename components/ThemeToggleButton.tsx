7a3868b35f34dfe88e462e1811307970d90d85bb
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-01 10:33:21 +0530

    Fix: Theme icon issue

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { moon, sun } from '@/assets';
+import { useEffect, useState } from 'react';
 
 const ThemeToggleButton = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
+  const [icon, setIcon] = useState(currentTheme === 'dark' ? sun : moon);
+
+  useEffect(() => {
+    setIcon(theme === 'dark' ? sun : moon);
+  }, [theme]);
 
   return (
     <button
       onClick={() => (theme === 'dark' ? setTheme('light') : setTheme('dark'))}
     >
       <Image
-        src={currentTheme === 'dark' ? sun : moon}
+        src={icon}
         alt='theme toggle image'
         className='aspect-square w-9'
       />
