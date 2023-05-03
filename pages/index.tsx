346ce6b82c617f589aad00ee9c48e1eff654eb53
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-03 17:42:36 +0530

    Add some padding to input

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main className='absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2'>
+      <main className='absolute left-1/2 top-1/2 w-full px-8 -translate-x-1/2 -translate-y-1/2'>
         <SearchInput />
       </main>
     </>
